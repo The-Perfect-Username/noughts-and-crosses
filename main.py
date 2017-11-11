fd7f645d9f0a7b013b2472e59954d6abd9a8d117
Lachlan Enderlin <lendo1995@gmail.com>
2017-11-11 22:03:23 +1000

    Created coordinates and input functionality

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 class Game:
     def __init__(self):
-        self.board = {i : None for i in range(0, 9)}
+        self.board = {i : None for i in range(1, 10)}
+        # self.sample = {}
+        self.coordinates = [
+                            {'x': 0, 'y': 0},
+                            {'x': 0, 'y': 7},
+                            {'x': 0, 'y': 12},
+                            {'x': 2, 'y': 2},
+                            {'x': 2, 'y': 7},
+                            {'x': 2, 'y': 12},
+                            {'x': 4, 'y': 2},
+                            {'x': 4, 'y': 7},
+                            {'x': 4, 'y': 12}
+                            ]
         self.player = None
         self.AI = None
         self.characters = ['X', 'O']
 
+    def start(self):
+        print("Choose a number between 1 and 9 to place your character on the board; left to right, top down. \n")
+        print(self.create_grid())
+        self.choose_character()
+
     def create_grid(self):
+        print(self.board)
         grid = ''
         for x in range(5):
             for y in range(14):
@@ -19,7 +37,27 @@ class Game:
                         grid += '|'
                     else:
                         grid += ' '
+
             grid += '\n'
+        return grid
+
+
+    def choose_character(self):
+        char = None
+        while True:
+            char = input("Choose a character (X or O): ")
+            if char.upper() in self.characters:
+                self.player = char
+                for c in self.characters:
+                    if self.player != c:
+                        self.AI = c
+                break
+
+    def get_player(self):
+        return self.player
+
+    def get_AI(self):
+        return self.AI
 
 
 # example_board = '''
@@ -39,14 +77,6 @@ class Game:
 #         ----|----|----
 #             |    |
 #     '''.format()
-#
-# board =
-#
-# print("Choose a number between 1 and 9 to place your character on the board")
-# print(example_board)
-#
-# character = input("Which character do you want? X or O ")
-# print ("You have chosen {}".format(character))
-# keep_playing = True
-#
-# while keep_playing:
+
+Game = Game()
+Game.start()
