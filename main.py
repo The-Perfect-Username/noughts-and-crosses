0b9e3e114cc0196f32f760b79daaca846b93fe11
Lachlan Enderlin <lendo1995@gmail.com>
2017-11-12 00:48:59 +1000

    Refactored grid creation and coordinates

--- main.py
+++ main.py
@@ -1,46 +1,40 @@
+import itertools
+
 class Game:
     def __init__(self):
-        self.board = {i : None for i in range(1, 10)}
-        # self.sample = {}
-        self.coordinates = [
-                            {'x': 0, 'y': 0},
-                            {'x': 0, 'y': 7},
-                            {'x': 0, 'y': 12},
-                            {'x': 2, 'y': 2},
-                            {'x': 2, 'y': 7},
-                            {'x': 2, 'y': 12},
-                            {'x': 4, 'y': 2},
-                            {'x': 4, 'y': 7},
-                            {'x': 4, 'y': 12}
-                            ]
+        self.board = {i : ' ' for i in range(1, 10)}
         self.player = None
         self.AI = None
         self.characters = ['X', 'O']
 
     def start(self):
         print("Choose a number between 1 and 9 to place your character on the board; left to right, top down. \n")
-        print(self.create_grid())
+        print(self.example_grid())
         self.choose_character()
 
     def create_grid(self):
-        print(self.board)
+        count = 1
         grid = ''
-        for x in range(5):
-            for y in range(14):
-                if (x % 2 == 1) or (x == 1):
-                    if (y == 4 or y == 9):
-                        grid += '|'
-                    else:
-                        grid += '-'
-                else:
-                    if (y == 4 or y == 9):
-                        grid += '|'
-                    else:
-                        grid += ' '
-
+        for y in range(5):
+            if (y == 0 or y % 2 == 0):
+                grid += "|".join([s.format(self.board[i]) for i, s in enumerate(list(itertools.repeat(" {} ", 3)), count)])
+                count += 3
+            else:
+                grid += "---+---+---"
             grid += '\n'
         return grid
 
+    def example_grid(self):
+        count = 1
+        grid = ''
+        for y in range(5):
+            if (y == 0 or y % 2 == 0):
+                grid += "|".join([s.format(i) for i, s in enumerate(list(itertools.repeat(" {} ", 3)), count)])
+                count += 3
+            else:
+                grid += "---+---+---"
+            grid += '\n'
+        return grid
 
     def choose_character(self):
         char = None
@@ -58,25 +52,6 @@ class Game:
 
     def get_AI(self):
         return self.AI
-
-
-# example_board = '''
-#      1  | 2  | 3
-#     ----|----|----
-#      4  | 5  | 6
-#     ----|----|----
-#      7  | 8  | 9
-# '''
-#
-#
-# def board():
-#     return '''
-#             |    |
-#         ----|----|----
-#             |    |
-#         ----|----|----
-#             |    |
-#     '''.format()
 
 Game = Game()
 Game.start()
