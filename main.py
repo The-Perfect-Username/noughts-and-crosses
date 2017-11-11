c5b2f905b8af463c5c1a9ec9acdc19745ff54dae
Lachlan Enderlin <lendo1995@gmail.com>
2017-11-12 01:34:21 +1000

    Started on the AI player. Able to see which possible combinations are available

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ class Game:
         self.player = None
         self.AI = None
         self.characters = ['X', 'O']
+        self.complete = False
+        self.combinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 6], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+        self.ai_memory = self.combinations
 
     def start(self):
         print("Choose a number between 1 and 9 to place your character on the board; left to right, top down. \n")
         print(self.example_grid())
         self.choose_character()
+        self.play()
 
     def create_grid(self):
         count = 1
@@ -47,11 +51,18 @@ class Game:
                         self.AI = c
                 break
 
-    def get_player(self):
-        return self.player
+    def play(self):
+        while not self.complete:
+            choice = int(input("Your turn: "))
+            self.board[choice] = self.player
+            print(self.create_grid())
+            print(self.ai_player())
 
-    def get_AI(self):
-        return self.AI
+    def ai_player(self):
+        tmp = [key for key, value in self.board.items() if value is self.player]
+        for i, j in enumerate(tmp):
+            self.ai_memory = [x for x in self.ai_memory if j not in x]
+
 
 Game = Game()
 Game.start()
